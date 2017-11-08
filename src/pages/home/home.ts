5e3953bc35c9882a52f1cf98a2c17d3099ba614e
Rafael Quines <rafael.quines@totvs.com.br>
2017-11-08 09:58:58 -0200

    dev

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ThfNotificationService } from '@totvs/thf-mobile/app/services/thf-notification/thf-notification.service';
-import { ThfToasterOrientation } from '@totvs/thf-mobile/app/services/thf-notification/thf-toaster-orientation';
+import { ThfToasterOrientation } from '@totvs/thf-core/services/thf-notification-base/thf-toaster-base/thf-toaster-orientation.enum';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
@@ -15,22 +15,22 @@ export class HomePage {
 
   callSuccess() {
     // console.log(ThfToasterOrientation.Bottom);
-    let toast = this.thfNotification.success({message: "Msg Sucesso", duration: 3000, orientation: ThfToasterOrientation.Top});
+    let toast = this.thfNotification.success("Msg Sucesso");
     toast.present();
   }
 
   callWarning() {
-    let toast = this.thfNotification.warning({message: "Msg Atenção", duration: 3000, orientation: ThfToasterOrientation.Bottom});
+    let toast = this.thfNotification.warning({message: "Msg Atenção", orientation: ThfToasterOrientation.Bottom});
     toast.present();
   }
 
   callError() {
-    let toast = this.thfNotification.error({message: "Msg Erro", duration: 3000, orientation: ThfToasterOrientation.Top});
+    let toast = this.thfNotification.error({message: "Msg Erro", orientation: ThfToasterOrientation.Top});
     toast.present();
   }
 
   callInformation() {
-    let toast = this.thfNotification.information({message: "Msg Informação", duration: 3000, orientation: ThfToasterOrientation.Bottom});
+    let toast = this.thfNotification.information({message: "Msg Informação", orientation: ThfToasterOrientation.Bottom});
     toast.present();
   }
 
